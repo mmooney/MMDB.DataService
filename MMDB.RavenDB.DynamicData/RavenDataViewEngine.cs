fb660c14df3d8ba54efc5f95729ea134d8442fcb
Mike Mooney <mmooney@mmdbsolutions.com>
2012-12-01 15:25:26 -0500

    Fixing build errors

--- MMDB.RavenDB.DynamicData/RavenDataViewEngine.cs
+++ MMDB.RavenDB.DynamicData/RavenDataViewEngine.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace MMDB.RavenDB.DynamicData
 {
-	public class RavenDataViewEngine : IViewEngine
-	{
-	}
+	//public class RavenDataViewEngine : IViewEngine
+	//{
+	//}
 }
