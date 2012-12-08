7395a9a335c34b9dbe674c918dbcacb289cb8e03
Mike Mooney <mmooney@mmdbsolutions.com>
2012-12-08 14:20:47 -0500

    Adding some X12 test classes

---
+++ MMDB.DataService.Data.Tests/X12/X12TestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMDB.DataService.Data.Tests.X12
+{
+	public abstract class X12TestBase
+	{
+		public X12Verifier Verifier { get; set; }
+
+		public X12TestBase(params char[] lineSeperators)
+		{
+			this.Verifier = new X12Verifier(lineSeperators);
+		}
+	}
+}
