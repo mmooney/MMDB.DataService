b21e203dfdd9a81b40f266ec866ec1ba94aba5b8
Mike Mooney <mmooney@mmdbsolutions.com>
2012-12-29 06:48:14 -0500

    Adding smtp username/password to razor email engine

--- MMDB.DataEmail/EmailSender.cs
+++ MMDB.DataEmail/EmailSender.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net.Mail;
+using System.Net;
 
 namespace MMDB.DataEmail
 {
@@ -17,12 +18,22 @@ namespace MMDB.DataEmail
 
 		public virtual void SendEmail(string subject, string body, IEnumerable<MailAddress> toAddressList, MailAddress fromAddress)
 		{
-			using(SmtpClient smtpClient = new SmtpClient(this.EmailServerSettings.Host, this.EmailServerSettings.Port))
+			int port = this.EmailServerSettings.Port.GetValueOrDefault(25);
+			using (var smtpClient = new SmtpClient(this.EmailServerSettings.Host, port))
 			{
+				if (!string.IsNullOrEmpty(this.EmailServerSettings.UserName))
+				{
+					var credential = new NetworkCredential(this.EmailServerSettings.UserName, this.EmailServerSettings.Password);
+					credential.GetCredential(smtpClient.Host, port, "Basic");
+					smtpClient.UseDefaultCredentials = false;
+					smtpClient.Credentials = credential;
+				}
+				
 				var message = new MailMessage();
 				message.From = fromAddress;
 				message.Body = body;
 				message.Subject = subject;
+				message.IsBodyHtml = true;
 				foreach(var toAddress in toAddressList)
 				{
 					message.To.Add(toAddress);
--- MMDB.DataEmail/EmailServerSettings.cs
+++ MMDB.DataEmail/EmailServerSettings.cs
@@ -8,6 +8,8 @@ namespace MMDB.DataEmail
 	public class EmailServerSettings
 	{
 		public string Host { get; set; }
-		public int Port { get; set; }
+		public int? Port { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
 	}
 }
