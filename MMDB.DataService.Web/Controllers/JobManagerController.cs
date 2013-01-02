5de5c67e39df3d52a71af35ff2b5a9bf9d5f5d33
Mike Mooney <mmooney@mmdbsolutions.com>
2013-01-01 22:14:17 -0500

    JobManager UI

--- MMDB.DataService.Web/Controllers/JobManagerController.cs
+++ MMDB.DataService.Web/Controllers/JobManagerController.cs
@@ -52,46 +52,32 @@ namespace MMDB.DataService.Web.Controllers
 			}
             return RedirectToAction("Index");
         }
-        
-        //
-        // GET: /JobManager/Edit/5
- 
-        public ActionResult Edit(int id)
+
+		public ActionResult Edit(int id)
+		{
+			var item = this.JobManager.LoadJob(id);
+			return View(item);
+		}        
+
+		[HttpPost]
+        public ActionResult Edit(int id, string jobName, string assemblyName, string className, string schedule, int intervalMinutes, int delayStartMinutes, string cronScheduleExpression)
         {
 			var item = this.JobManager.LoadJob(id);
-            return View(item);
-        }
+			if (schedule == "Simple")
+			{
+				this.JobManager.UpdateSimpleJob(id, jobName, assemblyName, className, intervalMinutes, delayStartMinutes);
+			}
+			else if (schedule == "CRON")
+			{
+				this.JobManager.UpdateCronJob(id, jobName, assemblyName, className, cronScheduleExpression);
+			}
+			else
+			{
+				throw new Exception("Unexpected schedule value: " + schedule);
+			}
+			return RedirectToAction("Index");
+		}
 
-        //
-        // POST: /JobManager/Edit/5
-
-        [HttpPost]
-		public ActionResult Create(int id, string jobName, string assemblyName, string className, string schedule, int intervalMinutes, int delayStartMinutes, string cronScheduleExpression)
-		{
-            try
-            {
-				var item = this.JobManager.LoadJob(id);
-				if(schedule == "Simple")
-				{
-					this.JobManager.UpdateSimpleJob(id, jobName, assemblyName, className, intervalMinutes, delayStartMinutes);
-				}
-				else if (schedule == "CRON")
-				{
-					this.JobManager.UpdateCronJob(id, jobName, assemblyName, className, cronScheduleExpression);
-				}
-				else 
-				{
-					throw new Exception("Unrecognized schedule " + schedule);
-				}
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
         // GET: /JobManager/Delete/5
  
         public ActionResult Delete(int id)
