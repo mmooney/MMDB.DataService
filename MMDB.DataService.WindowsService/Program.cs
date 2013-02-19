e9f8be1682bfda7024d889a6e2158cff66558dab
Mike Mooney <mmooney@mmdbsolutions.com>
2013-02-19 14:14:28 -0500

    Adding diagnostic logging

--- MMDB.DataService.WindowsService/Program.cs
+++ MMDB.DataService.WindowsService/Program.cs
@@ -22,26 +22,39 @@ namespace MMDB.DataService.WindowsService
 		/// </summary>
 		static void Main(string[] args)
 		{
-			NinjectBootstrapper.Initialize();
-			NinjectBootstrapper.Kernel.Bind<IDocumentStore>().ToMethod(CreateDocumentStore).InSingletonScope();
-			NinjectBootstrapper.Kernel.Bind<IDocumentSession>().ToMethod(c=>c.Kernel.Get<IDocumentStore>().OpenSession()).InTransientScope();
-			NinjectBootstrapper.Kernel.Bind<ISchedulerFactory>().To<StdSchedulerFactory>();
-			NinjectBootstrapper.Kernel.Bind<IScheduler>().ToMethod(CreateScheduler).InSingletonScope();
+			try
+			{
+				Console.WriteLine("Starting MMDB.DataService.WindowsService");
+				Console.WriteLine("\t-Initializing Ninject...");
+				NinjectBootstrapper.Initialize();
+				Console.WriteLine("\t-Binding components to Ninject...");
+				NinjectBootstrapper.Kernel.Bind<IDocumentStore>().ToMethod(CreateDocumentStore).InSingletonScope();
+				NinjectBootstrapper.Kernel.Bind<IDocumentSession>().ToMethod(c=>c.Kernel.Get<IDocumentStore>().OpenSession()).InTransientScope();
+				NinjectBootstrapper.Kernel.Bind<ISchedulerFactory>().To<StdSchedulerFactory>();
+				NinjectBootstrapper.Kernel.Bind<IScheduler>().ToMethod(CreateScheduler).InSingletonScope();
+				Console.WriteLine("\t-Ninject initialization complete...");
 			
-			var settings = NinjectBootstrapper.Kernel.Get<CoreDataServiceSettings>();
-			if (args.Length > 0 && args[0].ToLower() == "/debug")
-			{
-				var service = NinjectBootstrapper.Get<WinService>();
-				service.DebugStart();
+				var settings = NinjectBootstrapper.Kernel.Get<CoreDataServiceSettings>();
+				if (args.Length > 0 && args[0].ToLower() == "/debug")
+				{
+					Console.WriteLine("\t-Starting in debug mode...");
+					var service = NinjectBootstrapper.Get<WinService>();
+					service.DebugStart();
+				}
+				else 
+				{
+					Console.WriteLine("\t-Starting in service mode...");
+					ServiceBase[] ServicesToRun;
+					ServicesToRun = new ServiceBase[] 
+					{ 
+						NinjectBootstrapper.Get<WinService>() 
+					};
+					ServiceBase.Run(ServicesToRun);
+				}
 			}
-			else 
+			catch(Exception err)
 			{
-				ServiceBase[] ServicesToRun;
-				ServicesToRun = new ServiceBase[] 
-				{ 
-					NinjectBootstrapper.Get<WinService>() 
-				};
-				ServiceBase.Run(ServicesToRun);
+				Console.WriteLine("Error: " + err.ToString());
 			}
 		}
 
