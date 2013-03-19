45a79dbd6adfb64c52b93e21d488d15d26418f93
Mike Mooney <mmooney@mmdbsolutions.com>
2013-03-19 13:48:52 -0400

    test

---
+++ MMDB.DataService.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMDB.DataService.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login(string returnUrl)
+        {
+            if(string.IsNullOrEmpty(returnUrl))
+			{
+				return RedirectToAction("Index","Home");
+			}
+			else 
+			{
+				return Redirect(returnUrl);
+			}
+        }
+
+    }
+}
