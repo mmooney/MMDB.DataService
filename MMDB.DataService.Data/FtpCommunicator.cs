1f1d86c98a0bb96e22d6db3f3862b0d73791988a
Mike Mooney <mmooney@mmdbsolutions.com>
2013-06-19 10:53:29 -0400

    support for non-secure FTP

--- MMDB.DataService.Data/FtpCommunicator.cs
+++ MMDB.DataService.Data/FtpCommunicator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using MMDB.DataService.Data.Dto.Ftp;
 using MMDB.DataService.Data.Settings;
@@ -23,9 +24,6 @@ namespace MMDB.DataService.Data
 		public void UploadFile(EnumSettingSource settingSource, string settingKey, string targetFileName, string targetDirectory, byte[] fileData)
 		{
 			var settings = this.ConnectionSettingsManager.Load<FtpConnectionSettings>(settingSource, settingKey);
-			Tamir.SharpSsh.Sftp ftp = new Tamir.SharpSsh.Sftp(settings.FtpHost, settings.FtpUserName, settings.FtpPassword);
-			ftp.Connect();
-
 			string targetFilePath;
 			targetFileName = targetFileName.Replace("/", "_");
 			if (!string.IsNullOrEmpty(targetDirectory))
@@ -45,10 +43,37 @@ namespace MMDB.DataService.Data
 			string tempPath = Path.Combine(tempDirectory, Guid.NewGuid().ToString() + "." + targetFileName);
 			try
 			{
-				this.EventReporter.Info("Uploading file " + tempPath + " to " + targetFilePath + "...");
 				File.WriteAllBytes(tempPath, fileData);
-				ftp.Put(tempPath, targetFilePath);
-				this.EventReporter.Info("Uploading file " + tempPath + " to " + targetFilePath + " complete");
+
+				if(settings.SecureFTP)
+				{
+					this.EventReporter.Info("Uploading file " + tempPath + " to " + targetFilePath + "...");
+					Tamir.SharpSsh.Sftp ftp = new Tamir.SharpSsh.Sftp(settings.FtpHost, settings.FtpUserName, settings.FtpPassword);
+					ftp.Connect();
+					ftp.Put(tempPath, targetFilePath);
+					this.EventReporter.Info("Uploading file " + tempPath + " to " + targetFilePath + " complete");
+				}
+				else 
+				{
+					string targetUrl = Uri.UriSchemeFtp + settings.FtpHost + "/" + targetFilePath;
+					this.EventReporter.Info("Uploading file " + tempPath + " to " + targetUrl + "...");
+					FtpWebRequest request = (FtpWebRequest)WebRequest.Create(targetUrl);
+					request.Method = WebRequestMethods.Ftp.UploadFile;
+
+					request.Credentials = new NetworkCredential (settings.FtpUserName,settings.FtpPassword);
+
+					using(var requestStream = request.GetRequestStream())
+					using(var writer = new StreamWriter(requestStream))
+					{
+						writer.Write(fileData);
+					}
+					using(var response = (FtpWebResponse)request.GetResponse())
+					{
+						this.EventReporter.Info(string.Format("Upload File {0} Complete, status {1}", targetUrl, response.StatusDescription));
+						response.Close();
+					}
+				}
+
 			}
 			finally
 			{
